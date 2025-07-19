64bfc032f2786e34f359bdfe71fea33000c90442
Tinsae Tadesse <tinsaetadesse769@gmail.com>
2025-07-19 15:18:29 +0300

    updated db collection

--- app/db.py
+++ app/db.py
@@ -4,3 +4,4 @@ import os
 client = AsyncIOMotorClient(os.getenv("MONGODB_URL"))
 db = client[os.getenv("DB_NAME", "financial_ocr_db")]
 users_collection = db.get_collection("users")
+documents_collection = db.get_collection("documents")
